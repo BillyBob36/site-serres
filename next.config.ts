d80efdc0b2e3d1b40d6884fdd75a85ae3e37a85c
BillyBob36 <105729899+BillyBob36@users.noreply.github.com>
2026-03-23 22:27:54 +0400

    Configure static export for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["127.0.0.1", "localhost"],
+  output: "export",
+  basePath: "/site-serres",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
